8c9620fe80c789c5b2fd957a1f3364d09252084d
tasi <taegyung.e@gmail.com>
2025-08-21 16:11:57 +0900

    fix(twMerge): 커스텀한 테마 변수를 twMerge시 제대로 병합되지 않는 이슈 수정

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,6 @@
+import twMerge from "@/utils/customTwMerge";
 import clsx from "clsx";
 import { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
