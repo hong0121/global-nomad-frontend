aecbbeeb2919575892a17b3da70c0a6b92ebf5b6
tasi <taegyung.e@gmail.com>
2025-08-21 17:32:10 +0900

    style(Font): 폰트 패밀리 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import { pretendard } from "@/styles/fonts";
 
 export const metadata: Metadata = {
-  title: 'Global Nomad',
-  description: 'Global Nomad Project',
+  title: "Global Nomad",
+  description: "Global Nomad Project",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='ko'>
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
