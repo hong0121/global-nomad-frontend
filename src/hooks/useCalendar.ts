4065ec2e7314c1d9d1e2cc18379bf5a40a205941
Squarecat-meow <yozumina@serafuku.moe>
2025-08-21 23:59:23 +0900

    feat(useCalendar): useCalendar 훅 작성

---
+++ src/hooks/useCalendar.ts
@@ -0,0 +1,16 @@
+import { getDaysArray } from '@/app/(global)/detail/[id]/_utils/getDaysArray';
+import { useState } from 'react';
+
+export function useCalendar() {
+  const today = new Date(Date.now());
+  const [selectedDate, setSelectedDate] = useState<Date>(today);
+  const [dateToDisplay, setDateToDisplay] = useState<Date>(today);
+
+  const daysArray = getDaysArray(dateToDisplay);
+
+  return {
+    daysArray,
+    dateSelector: { selectedDate, setSelectedDate },
+    displayController: { dateToDisplay, setDateToDisplay },
+  };
+}
