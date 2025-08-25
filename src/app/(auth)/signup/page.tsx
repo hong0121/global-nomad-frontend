6a7ab737f83cfb73cd9206b0a093aafe09af76be
tasi <taegyung.e@gmail.com>
2025-08-25 15:57:11 +0900

    feat(KakaoSignup): 카카오 소셜 회원가입 API 연동

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,17 @@
-import SignupForm from '@/src/app/(auth)/signup/_components/SignupForm';
+import AuthFormHelper from '@/src/components/primitive/auth/AuthFormHelper';
+import KakaoSignupButton from '@/src/components/pages/signup/KakaoSignupButton';
+import SignupForm from '@/src/components/pages/signup/SignupForm';
 
 export default function SignupPage() {
   return (
     <div className='max-w-[640px] mx-auto my-[30px]'>
       <SignupForm />
+      <KakaoSignupButton />
+      <AuthFormHelper
+        mainText='회원이신가요?'
+        linkText='로그인하기'
+        linkUrl='/login'
+      />
     </div>
   );
 }
