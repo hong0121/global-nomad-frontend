dd18d6e1eef538efc24d67e7224229a95718512d
sinyroom <wldmsdl7913@naver.com>
2025-08-26 20:02:58 +0900

    feat: 체험 리뷰 임시 데이터 추가

--- src/components/pages/detail/ActivityDetail.tsx
+++ src/components/pages/detail/ActivityDetail.tsx
@@ -9,7 +9,49 @@ interface Props {
 }
 
 export default function ActivityDetail({ activity, reviewData }: Props) {
-  const reviews = reviewData.reviews;
+  // 임시 데이터
+  const mockreviewData: ReviewResponse = {
+    averageRating: 4.5,
+    totalCount: 4,
+    reviews: [
+      {
+        id: 1,
+        user: { profileImageUrl: '', nickname: '김철수', id: 1 },
+        activityId: activity.id,
+        rating: 4,
+        content: '처음 체험이었지만 강사님이 친절하게 알려주셔서 즐거웠습니다!',
+        createdAt: '2025-08-26T10:25:17.776Z',
+        updatedAt: '2025-08-26T10:25:17.776Z',
+      },
+      {
+        id: 2,
+        user: { profileImageUrl: '', nickname: '이영희', id: 2 },
+        activityId: activity.id,
+        rating: 5,
+        content: '친구들과 함께 와서 너무 즐거운 시간을 보냈습니다.',
+        createdAt: '2025-08-25T09:15:10.000Z',
+        updatedAt: '2025-08-25T09:15:10.000Z',
+      },
+      {
+        id: 3,
+        user: { profileImageUrl: '', nickname: '박민수', id: 3 },
+        activityId: activity.id,
+        rating: 4,
+        content: '물 위에서 여유롭게 즐길 수 있었고 안전했습니다.',
+        createdAt: '2025-08-24T14:40:00.000Z',
+        updatedAt: '2025-08-24T14:40:00.000Z',
+      },
+      {
+        id: 4,
+        user: { profileImageUrl: '', nickname: '최지은', id: 4 },
+        activityId: activity.id,
+        rating: 4.8,
+        content: '사진 찍기 좋은 포인트가 많아 추억 남기기 좋았습니다!',
+        createdAt: '2025-08-23T11:20:00.000Z',
+        updatedAt: '2025-08-23T11:20:00.000Z',
+      },
+    ],
+  };
 
   return (
     <>
@@ -46,7 +88,7 @@ export default function ActivityDetail({ activity, reviewData }: Props) {
           </p>
           <div className='flex gap-0.5'>
             <div className='relative w-4 h-4'>
-              <Image src='/images/icons/StarIcon.svg' alt='체험별점' fill />
+              <Image src='/images/icons/StarFilled.svg' alt='체험별점' fill />
             </div>
             <span className='text-gray-500 text-14 font-medium'>
               {activity.reviewCount}개의 후기
@@ -55,26 +97,33 @@ export default function ActivityDetail({ activity, reviewData }: Props) {
         </div>
 
         {/* 리뷰 3개씩 목록으로 표시 (예정) */}
-        <article className='w-full rounded-3xl p-5 shadow-[0px_4px_24px_0px_#9CB4CA] mb-5'>
-          <h3 className='inline-block text-14 font-semibold mb-2 md:text-16 md:font-bold'>
-            작성자명{' '}
-            <span className='text-gray-300 text-12 font-semibold md:text-14 md:font-medium'>
-              작성일자
-            </span>
-          </h3>
-          <div className='relative w-4 h-4 mb-2'>
-            <Image src='/images/icons/StarIcon.svg' alt='체험별점' fill />
-          </div>
-          <p className='text-14 font-medium md:text-16'>
-            저는 저희 스트릿 댄서 체험에 참가하게 된 지 얼마 안됐지만, 정말
-            즐거운 시간을 보냈습니다. 새로운 스타일과 춤추기를 좋아하는 나에게
-            정말 적합한 체험이었고, 전문가가 직접 강사로 참여하기 때문에 어떤
-            수준의 춤추는 사람도 쉽게 이해할 수 있었습니다. 강사님께서 정말
-            친절하게 설명해주셔서 정말 좋았고, 이번 체험을 거쳐 새로운 스타일과
-            춤추기에 대한 열정이 더욱 생겼습니다. 저는 이 체험을 적극
-            추천합니다!"
-          </p>
-        </article>
+        {mockreviewData.reviews.map((review) => (
+          <article
+            key={review.id}
+            className='w-full rounded-3xl p-5 shadow-[0px_4px_24px_0px_#dde6ef] mb-5'
+          >
+            <h3 className='inline-block text-14 font-semibold mb-2 md:text-16 md:font-bold'>
+              {review.user.nickname}{' '}
+              <span className='text-gray-300 text-12 font-semibold md:text-14 md:font-medium'>
+                {new Date(review.createdAt)
+                  .toLocaleDateString()
+                  .replace(/\.$/, '')}
+              </span>
+            </h3>
+            <div className='flex md:mb-2.5'>
+              {Array.from({ length: review.rating }, (_, i) => (
+                <div className='relative w-4 h-4 mb-2'>
+                  <Image
+                    src='/images/icons/StarFilled.svg'
+                    alt='체험별점'
+                    fill
+                  />
+                </div>
+              ))}
+            </div>
+            <p className='text-14 font-medium md:text-16'>{review.content}</p>
+          </article>
+        ))}
 
         {/* 임시 페이지네이션 UI */}
         <div className='flex justify-center gap-2 w-full mt-10 mb-20'>
