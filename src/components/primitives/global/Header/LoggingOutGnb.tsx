9a07d8091fa4baada9612c521d31e3ee440c8c2a
Squarecat-meow <yozumina@serafuku.moe>
2025-08-27 18:16:35 +0900

    feat(Modal): 알림 모달 구현

--- src/components/primitives/global/Header/LoggingOutGnb.tsx
+++ src/components/primitives/global/Header/LoggingOutGnb.tsx
@@ -1,23 +1,35 @@
 import Image from 'next/image';
+import BellIcon from '@/public/images/icons/Bell.svg';
+import { useState } from 'react';
+import NotificationModal from '../../notification/NotificationModal';
 
 export default function LoggingOutGnb() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
-    <div className='flex items-center gap-5'>
-      <div className='relative'>
-        <button>알림 아이콘</button>
-      </div>
-      <span className='w-[1px] h-[14px] bg-gray-100' />
-      <div className='relative'>
-        <button className='flex items-center gap-[10px]'>
-          <Image
-            src='/images/UserDefaultImg.svg'
-            width={30}
-            height={30}
-            alt='유저 기본 이미지'
-          />
-          <strong className='text-14 font-medium'>홍길동</strong>
+    <div className='relative'>
+      <div className='flex items-center gap-5'>
+        <button className='relative' onClick={() => setIsModalVisible(true)}>
+          <BellIcon />
         </button>
+        <span className='w-[1px] h-[14px] bg-gray-100' />
+        <div className='relative'>
+          <button className='flex items-center gap-[10px]'>
+            <Image
+              src='/images/UserDefaultImg.svg'
+              width={30}
+              height={30}
+              alt='유저 기본 이미지'
+            />
+            <strong className='text-14 font-medium'>홍길동</strong>
+          </button>
+        </div>
       </div>
+      <NotificationModal
+        visible={isModalVisible}
+        setVisible={setIsModalVisible}
+        className='absolute top-10 gnb:top-10 right-11 gnb:left-[82.5%]'
+      />
     </div>
   );
 }
