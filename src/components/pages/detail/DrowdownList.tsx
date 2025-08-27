59da3adda1a2d73a3c854e3c3e623535f14b23e5
sinyroom <wldmsdl7913@naver.com>
2025-08-27 18:23:00 +0900

    feat(DropdownList): 체험 수정하기로 리다이렉트 연결

--- src/components/pages/detail/DrowdownList.tsx
+++ src/components/pages/detail/DrowdownList.tsx
@@ -1,11 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function DropdownList() {
+  const router = useRouter();
+
+  const handleUpdate = () => {
+    router.push('/myCreateExperiences');
+  };
+
   return (
     <>
       <div className='flex flex-col justify-around w-24 h-27 border border-[#dfdfdf] rounded-[8px] shadow-[0px_0px_3px_0px_#dde6ef]'>
-        <button className='text-16 font-medium' aria-label='수정하기'>
+        <button
+          onClick={handleUpdate}
+          type='button'
+          className='text-16 font-medium'
+          aria-label='수정하기'
+        >
           수정하기
         </button>
-        <button className='text-16 font-medium' aria-label='삭제하기'>
+        <button
+          type='button'
+          className='text-16 font-medium'
+          aria-label='삭제하기'
+        >
           삭제하기
         </button>
       </div>
