8a9db77587743212dc9ed81b32a74fd21d5b10a2
sinyroom <wldmsdl7913@naver.com>
2025-08-28 14:17:06 +0900

    feat(API): 체험 삭제 API 연동

--- src/services/pages/detail/activity.ts
+++ src/services/pages/detail/activity.ts
@@ -2,11 +2,18 @@ import { Activity } from '@/src/types/activityType';
 import { apiClient } from '../../primitives/apiClient';
 
 /**
- * 특정 activityId의 체험 상세 조회
  * @param activityId 체험 ID
  * @returns Activity 객체
  */
+
+// 특정 activityId의 체험 상세 조회
 export async function getActivityById(activityId: number): Promise<Activity> {
   const { data } = await apiClient.get<Activity>(`/activities/${activityId}`);
   return data;
+}
+
+// 특정 activityId의 체험 삭제
+export async function deleteActivityById(activityId: number) {
+  const res = await apiClient.delete(`/my-activities/${activityId}`);
+  return res;
 }
