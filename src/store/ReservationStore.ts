fc7bac0ced41f30d6dc7458f80d34e3eeb8d0481
Squarecat-meow <yozumina@serafuku.moe>
2025-08-29 12:29:08 +0900

    fix(ReservationStore): zustand에 예약에 필요한 데이터들 store 작성

---
+++ src/store/ReservationStore.ts
@@ -0,0 +1,54 @@
+import { create } from 'zustand';
+
+interface IReservationStore {
+  dateSelector: {
+    selectedDate: Date;
+    setSelectedDate: (date: Date) => void;
+  };
+  personSelector: {
+    person: number;
+    incrementPerson: () => void;
+    decrementPerson: () => void;
+  };
+  timeSelector: {
+    timeId: number | null;
+    setTimeId: (id: number) => void;
+  };
+}
+
+export const useReservationStore = create<IReservationStore>()((set) => ({
+  dateSelector: {
+    selectedDate: new Date(),
+    setSelectedDate: (by) =>
+      set((state) => ({
+        dateSelector: { ...state.dateSelector, selectedDate: by },
+      })),
+  },
+  personSelector: {
+    person: 1,
+    incrementPerson: () =>
+      set((state) => ({
+        personSelector: {
+          ...state.personSelector,
+          person: state.personSelector.person + 1,
+        },
+      })),
+    decrementPerson: () =>
+      set((state) => ({
+        personSelector: {
+          ...state.personSelector,
+          person: state.personSelector.person - 1,
+        },
+      })),
+  },
+  timeSelector: {
+    timeId: null,
+    setTimeId: (by) =>
+      set((state) => ({
+        timeSelector: {
+          ...state.timeSelector,
+          timeId: by,
+        },
+      })),
+  },
+}));
