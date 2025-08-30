d5eabb14603a658c672c21843439d5d979a5579b
Seonghyun Hong <shfo18@naver.com>
2025-08-30 14:32:39 +0900

    fix: InputUI 피드백 반영

--- src/components/primitives/input/FormInput.tsx
+++ src/components/primitives/input/FormInput.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { forwardRef, InputHTMLAttributes } from 'react';
-import clsx from 'clsx';
 import PasswordInput from './PasswordInput';
 import Input from './Input';
+import { cn } from '@/src/utils/cn';
 
 interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -14,7 +14,7 @@ interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
   ({ label, errorMessage, isPassword, variant = 'default', ...props }, ref) => {
-    const variantLabelStyles = clsx({
+    const variantLabelStyles = cn({
       'font-bold text-base text-gray-950': variant === 'default',
       'font-medium text-base text-gray-950': variant === 'experience',
       'font-medium text-lg text-text-gray-950': variant === 'auth',
@@ -32,20 +32,17 @@ const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
           <PasswordInput
             ref={ref}
             {...props}
-            className={clsx(baseBorder, focusBorder, errorBorder)}
+            className={cn(baseBorder, focusBorder, errorBorder)}
           />
         ) : (
           <Input
             ref={ref}
             {...props}
-            className={clsx(baseBorder, focusBorder, errorBorder)}
+            className={cn(baseBorder, focusBorder, errorBorder)}
           />
         )}
         <span
-          className={clsx(
-            'text-sm min-h-5 pl-2',
-            errorMessage ? 'text-red-500 visible' : 'invisible'
-          )}
+          className={cn('text-sm pl-2', errorMessage ? 'text-red-500' : null)}
         >
           {errorMessage || ' '}
         </span>
