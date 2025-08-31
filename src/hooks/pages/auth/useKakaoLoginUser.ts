e8dd00009c9e562f0352364874ba2b30597a69a8
tasi <taegyung.e@gmail.com>
2025-09-01 02:14:03 +0900

    feat(login): 보호된 페이지에 로그인 하지 않은 사용자가 접속시 로그인 페이지로 리다이렉트, 로그인 성공시 접속을 시도했던 페이지로 다시 리다이렉트 되도록 처리

--- src/hooks/pages/auth/useKakaoLoginUser.ts
+++ src/hooks/pages/auth/useKakaoLoginUser.ts
@@ -6,11 +6,13 @@ import { queries } from '@/src/services/primitives/queries';
 import { setTokenAction } from '@/src/services/primitives/tokenAction';
 import { TokenUserResponseType } from '@/src/types/userType';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function useKakaoLoginUser() {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const searchParams = useSearchParams();
+  const redirectPath = searchParams.get('state');
 
   return useMutation({
     mutationFn: async (data: KakaoLoginRequestBody) =>
@@ -19,7 +21,13 @@ export default function useKakaoLoginUser() {
       queryClient.setQueryData(queries.user(), data.user); // 리액트 쿼리 데이터 캐싱
       localStorage.setItem('accessToken', data.accessToken); // 액세스 토큰 로컬스토리지 저장
       await setTokenAction(data.refreshToken); // 리프레시 토큰 쿠키 설정
-      router.replace('/');
+
+      // redirectPath 값이 있으면, 해당 페이지로 다시 이동
+      if (redirectPath) {
+        router.replace(redirectPath);
+      } else {
+        router.replace('/');
+      }
     },
     onError: () => {
       alert('로그인에 실패하였습니다. 다시 시도 해주세요.');
--- src/hooks/pages/auth/useLoginUser.ts
+++ src/hooks/pages/auth/useLoginUser.ts
@@ -4,11 +4,13 @@ import { setTokenAction } from '@/src/services/primitives/tokenAction';
 import { TokenUserResponseType } from '@/src/types/userType';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function useLoginUser() {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const searchParams = useSearchParams();
+  const redirectPath = searchParams.get('redirect_path');
 
   return useMutation({
     mutationFn: (data: loginRequestBody) => loginUser(data),
@@ -16,7 +18,13 @@ export default function useLoginUser() {
       queryClient.setQueryData(queries.user(), data.user); // 리액트 쿼리 데이터 캐싱
       localStorage.setItem('accessToken', data.accessToken); // 액세스 토큰 로컬스토리지 저장
       await setTokenAction(data.refreshToken); // 리프레시 토큰 쿠키 설정
-      router.replace('/');
+
+      // redirectPath 값이 있으면, 해당 페이지로 다시 이동
+      if (redirectPath) {
+        router.replace(redirectPath);
+      } else {
+        router.replace('/');
+      }
     },
     onError: (error) => {
       const err = error as AxiosError<{ message: string }>;
