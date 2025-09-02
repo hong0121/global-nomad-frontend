3b1c2b8eb7b6c975c45b7eed701a3eb8badec099
tasi <taegyung.e@gmail.com>
2025-09-03 02:03:55 +0900

    feat(Signup): 회원가입 페이지 RHF 적용 및 UI 완료

--- src/components/pages/signup/SignupForm.tsx
+++ src/components/pages/signup/SignupForm.tsx
@@ -1,69 +1,161 @@
 'use client';
 import Button from '@/src/components/primitives/Button';
+import FormInput from '@/src/components/primitives/input/FormInput';
+import AlertModal from '@/src/components/primitives/modal/AlertModal';
 import { createUser } from '@/src/services/pages/signup/api';
 import { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface FormDataType {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+const EMAIL_REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const alertInit = {
+  open: false,
+  msg: '',
+  onClose: () => {},
+};
 
 export default function SignupForm() {
   const router = useRouter();
+  const [alertModal, setAlertModal] = useState(alertInit);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    trigger,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<FormDataType>({ mode: 'onBlur' });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get('email')?.toString();
-    const nickname = formData.get('nickname')?.toString();
-    const password = formData.get('password')?.toString();
-
-    if (!email || !nickname || !password) return;
+  const handleSubmitForm = async (formData: FormDataType) => {
+    const { email, nickname, password } = formData;
 
     try {
       await createUser({ email, nickname, password });
-      router.replace('/login'); // 성공하면 로그인 페이지로 이동
+
+      setAlertModal({
+        open: true,
+        msg: '가입이 완료되었습니다.',
+        onClose: () => {
+          setAlertModal(alertInit);
+          router.replace('/login'); // 성공하면 로그인 페이지로 이동
+        },
+      });
     } catch (error) {
       const err = error as AxiosError<{ message: string }>;
 
       if (err.response?.status === 409) {
         // 중복 이메일 체크
-        alert(err.response?.data?.message); // 추후에, 토스트 UI로 교체 예정
+        setAlertModal({
+          open: true,
+          msg: '이미 사용 중인 이메일입니다.',
+          onClose: () => setAlertModal(alertInit),
+        });
         return;
       }
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='mb-2'>
-        <label className='block'>이메일</label>
-        <input name='email' type='text' placeholder='이메일을 입력해 주세요' />
-      </div>
-      <div className='mb-2'>
-        <label className='block'>닉네임</label>
-        <input
-          name='nickname'
+    <>
+      <form
+        onSubmit={handleSubmit(handleSubmitForm)}
+        className='flex flex-col gap-4 md:gap-5'
+      >
+        <FormInput
+          label='이메일'
+          variant='auth'
+          type='text'
+          placeholder='이메일을 입력해 주세요'
+          errorMessage={errors.email?.message}
+          {...register('email', {
+            required: '이메일을 입력해 주세요',
+            pattern: {
+              value: EMAIL_REGEXP,
+              message: '이메일 형식으로 작성해 주세요.',
+            },
+            setValueAs: (v) => v.trim(),
+          })}
+        />
+
+        <FormInput
+          label='닉네임'
+          variant='auth'
           type='text'
           placeholder='닉네임을 입력해 주세요'
+          errorMessage={errors.nickname?.message}
+          {...register('nickname', {
+            required: '닉네임을 입력해 주세요',
+            maxLength: {
+              value: 10,
+              message: '10자 이하로 작성해주세요.',
+            },
+            setValueAs: (v) => v.trim(),
+          })}
         />
-      </div>
-      <div className='mb-2'>
-        <label className='block'>비밀번호</label>
-        <input
-          name='password'
+
+        <FormInput
+          label='비밀번호'
+          isPassword
+          variant='auth'
           type='password'
-          placeholder='8자 이상 입력해 주세요'
+          placeholder='비밀번호를 입력해 주세요'
+          errorMessage={errors.password?.message}
+          {...register('password', {
+            required: '비밀번호를 입력해 주세요',
+            minLength: {
+              value: 8,
+              message: '8자 이상 입력해주세요.',
+            },
+            onBlur: () => {
+              const passwordConfirm = getValues('passwordConfirm');
+              if (passwordConfirm) trigger('passwordConfirm');
+            },
+            setValueAs: (v) => v.trim(),
+          })}
         />
-      </div>
-      <div className='mb-2'>
-        <label className='block'>비밀번호 확인</label>
-        <input
+
+        <FormInput
+          label='비밀번호'
+          isPassword
+          variant='auth'
           type='password'
           placeholder='비밀번호를 한 번 더 입력해 주세요'
+          errorMessage={errors.passwordConfirm?.message}
+          {...register('passwordConfirm', {
+            required: '비밀번호를 입력해 주세요',
+            validate: (value) => {
+              const password = getValues('password');
+              if (value !== password) return '비밀번호가 일치하지 않습니다.';
+              return true;
+            },
+            setValueAs: (v) => v.trim(),
+          })}
         />
-      </div>
-      <Button size='lg' full>
-        회원가입하기
-      </Button>
-    </form>
+
+        <Button
+          size='lg'
+          full
+          className='mt-2 md:mt-[10px]'
+          disabled={!isValid || isSubmitting}
+        >
+          회원가입하기
+        </Button>
+      </form>
+      <AlertModal
+        isOpen={alertModal.open}
+        message={alertModal.msg}
+        onClose={alertModal.onClose}
+      />
+    </>
   );
 }
