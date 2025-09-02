1040305036ae6f7c9e426949c6acef3ac616bdb1
sinyroom <wldmsdl7913@naver.com>
2025-09-02 18:24:55 +0900

    feat(Image): 체험 이미지 연결

--- src/components/pages/detail/ActivityImage.tsx
+++ src/components/pages/detail/ActivityImage.tsx
@@ -8,12 +8,11 @@ interface Props {
 export default function ActivityImage({ activity }: Props) {
   return (
     <>
-      {/* 체험 이미지 (추후 교체 예정) */}
       <section className='flex gap-1.5'>
         <div className='relative w-1/2 h-[246px] md:h-[400px]'>
           <Image
-            src='/images/Dancing.jpg'
-            alt='체험이미지'
+            src={activity.bannerImageUrl}
+            alt={activity.title}
             fill
             className='object-cover object-center rounded-tl-3xl rounded-bl-3xl'
             sizes='(max-width: 744px) 100vw, 50vw'
@@ -21,24 +20,23 @@ export default function ActivityImage({ activity }: Props) {
           />
         </div>
         <div className='flex flex-col gap-1.5 w-1/2'>
-          <div className='relative w-full h-30 md:h-[197px]'>
-            <Image
-              src='/images/Dancing2.jpg'
-              alt='체험이미지'
-              fill
-              className='object-cover object-center rounded-tr-3xl'
-              sizes='(max-width: 744px) 100vw, 50vw'
-            />
-          </div>
-          <div className='relative w-full h-30 md:h-[197px]'>
-            <Image
-              src='/images/Dancing3.jpg'
-              alt='체험이미지'
-              fill
-              className='object-cover object-center rounded-br-3xl'
-              sizes='(max-width: 744px) 100vw, 50vw'
-            />
-          </div>
+          {activity.subImages.map((img, idx) => (
+            <div key={img.id} className='relative w-full h-30 md:h-[197px]'>
+              <Image
+                src={img.imageUrl}
+                alt={activity.title}
+                fill
+                className={`object-cover object-center ${
+                  idx === 0
+                    ? 'rounded-tr-3xl'
+                    : idx === 1
+                    ? 'rounded-br-3xl'
+                    : ''
+                }`}
+                sizes='(max-width: 744px) 100vw, 50vw'
+              />
+            </div>
+          ))}
         </div>
       </section>
     </>
