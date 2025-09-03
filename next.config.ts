d1c0ffd9f9eddc10595655bb7c76bdb796b7f2e6
tasi <taegyung.e@gmail.com>
2025-09-03 15:26:57 +0900

    chore(config): next config 파일에 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  images: {
+    remotePatterns: [
+      new URL(
+        'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/**'
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
