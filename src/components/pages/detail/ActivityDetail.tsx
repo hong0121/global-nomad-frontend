a28d39db6a3610bc3bff3f8b7b4f7b148fc0e38f
sinyroom <wldmsdl7913@naver.com>
2025-09-04 15:04:47 +0900

    feat(Review): 리뷰 컴포넌트 분리, 임시데이터 정리

--- src/components/pages/detail/ActivityDetail.tsx
+++ src/components/pages/detail/ActivityDetail.tsx
@@ -3,9 +3,9 @@ import RatingText from './RatingText';
 import Map from './Map';
 import { useState } from 'react';
 import { REVIEWS_PER_PAGE } from '@/src/constants/pagination';
-import { Review, ReviewResponse } from '@/src/types/reviewType';
+import { ReviewResponse } from '@/src/types/reviewType';
 import { Activity } from '@/src/types/activityType';
-import { format } from 'date-fns';
+import ReviewList from './ReviewList';
 
 interface Props {
   activity: Activity;
@@ -13,61 +13,12 @@ interface Props {
 }
 
 export default function ActivityDetail({ activity, reviewData }: Props) {
-  // 임시 데이터
-  const mockreviewData: ReviewResponse = {
-    averageRating: 4.5,
-    totalCount: 4,
-    reviews: [
-      {
-        id: 1,
-        user: { profileImageUrl: '', nickname: '김철수', id: 1 },
-        activityId: activity.id,
-        rating: 4,
-        content: '처음 체험이었지만 강사님이 친절하게 알려주셔서 즐거웠습니다!',
-        createdAt: '2025-08-26T10:25:17.776Z',
-        updatedAt: '2025-08-26T10:25:17.776Z',
-      },
-      {
-        id: 2,
-        user: { profileImageUrl: '', nickname: '이영희', id: 2 },
-        activityId: activity.id,
-        rating: 5,
-        content: '친구들과 함께 와서 너무 즐거운 시간을 보냈습니다.',
-        createdAt: '2025-08-25T09:15:10.000Z',
-        updatedAt: '2025-08-25T09:15:10.000Z',
-      },
-      {
-        id: 3,
-        user: { profileImageUrl: '', nickname: '박민수', id: 3 },
-        activityId: activity.id,
-        rating: 4,
-        content: '물 위에서 여유롭게 즐길 수 있었고 안전했습니다.',
-        createdAt: '2025-08-24T14:40:00.000Z',
-        updatedAt: '2025-08-24T14:40:00.000Z',
-      },
-      {
-        id: 4,
-        user: { profileImageUrl: '', nickname: '최지은', id: 4 },
-        activityId: activity.id,
-        rating: 4.8,
-        content: '사진 찍기 좋은 포인트가 많아 추억 남기기 좋았습니다!',
-        createdAt: '2025-08-23T11:20:00.000Z',
-        updatedAt: '2025-08-23T11:20:00.000Z',
-      },
-    ],
-  };
-
   // 페이지네이션
   const [currentPage, setCurrentPage] = useState(1);
-  const reviewsPerPage = REVIEWS_PER_PAGE;
-  // const totalPages = Math.ceil(reviewData.reviews.length / reviewsPerPage);
-  // 임시 데이터
-  const totalPages = Math.ceil(mockreviewData.reviews.length / reviewsPerPage);
-
-  const currentReviews = mockreviewData.reviews.slice(
-    // const currentReviews = reviewData.reviews.slice(
-    (currentPage - 1) * reviewsPerPage,
-    currentPage * reviewsPerPage
+  const totalPages = Math.ceil(reviewData.reviews.length / REVIEWS_PER_PAGE);
+  const currentReviews = reviewData.reviews.slice(
+    (currentPage - 1) * REVIEWS_PER_PAGE,
+    currentPage * REVIEWS_PER_PAGE
   );
 
   return (
@@ -114,40 +65,10 @@ export default function ActivityDetail({ activity, reviewData }: Props) {
         </div>
 
         {/* 리뷰 목록 */}
-        {/* 임시 데이터 */}
-        {currentReviews.map((review: Review) => (
-          // {reviewData.reviews.map((review: Review) => (
-          <article
-            key={review.id}
-            className='w-full rounded-3xl p-5 shadow-[0px_4px_24px_0px_#dde6ef] mb-10 not-even:md:mb-5'
-          >
-            <h3 className='inline-block text-14 font-semibold mb-2 md:text-16 md:font-bold'>
-              {review.user.nickname}{' '}
-              <span className='text-gray-300 text-12 font-semibold md:text-14 md:font-medium'>
-                {format(
-                  review.updatedAt ? review.updatedAt : review.createdAt,
-                  'yyyy. M. dd'
-                )}
-              </span>
-            </h3>
-            <div className='flex md:mb-2.5'>
-              {Array.from({ length: review.rating }, (_, i) => (
-                <div key={i} className='relative w-4 h-4 mb-2'>
-                  <Image
-                    src='/images/icons/StarFilled.svg'
-                    alt='체험별점'
-                    fill
-                  />
-                </div>
-              ))}
-            </div>
-            <p className='text-14 font-medium md:text-16'>{review.content}</p>
-          </article>
-        ))}
+        <ReviewList reviews={currentReviews} />
 
-        {/* 임시 페이지네이션 UI */}
-        {mockreviewData.reviews.length > REVIEWS_PER_PAGE && (
-          // {reviewData.reviews.length > REVIEWS_PER_PAGE && (
+        {/* 임시 페이지네이션 UI  */}
+        {reviewData.reviews.length > REVIEWS_PER_PAGE && (
           <div className='flex justify-center gap-2 w-full mt-10 mb-20'>
             <button
               disabled={currentPage === 1}
