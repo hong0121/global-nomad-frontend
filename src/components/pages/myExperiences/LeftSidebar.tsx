7b43d4275b65b960781b6aca432f979af887583d
Squarecat-meow <yozumina@serafuku.moe>
2025-09-04 17:44:26 +0900

    fix(style): rounded-xl => 12px로  변경

--- src/components/pages/myExperiences/LeftSidebar.tsx
+++ src/components/pages/myExperiences/LeftSidebar.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/src/utils/cn';
 import ProfilePicture from './ProfilePicture';
 import SidebarTabs from '../sidebar/Tabs';
 import { ISidebarButtons } from '@/src/app/(global)/(mypage)/(sidebar)/layout';
+import { SIDEBAR_WIDTH } from '@/src/constants/myExperiences';
 
 export default function LeftSidebar({
   currentTab,
@@ -16,14 +17,16 @@ export default function LeftSidebar({
 }) {
   const buttonsStyle = {
     buttonClass:
-      'w-full h-[54px] flex gap-2 items-center py-3 pl-5 text-gray-600 rounded-2xl transition-colors hover:bg-primary-100',
+      'w-full h-[54px] flex gap-2 items-center py-3 pl-5 text-gray-600 rounded-[16px] transition-colors hover:bg-primary-100',
     buttonActiveClass: 'text-gray-950 bg-primary-100',
     iconClass: 'fill-gray-600',
     iconActiveClass: 'fill-primary-500',
   };
 
   return (
-    <aside className='shadow min-w-[290px] h-fit px-3.5 py-6 flex flex-col items-center rounded-2xl'>
+    <aside
+      className={`shadow-[0px_4px_24px_rgba(156,180,202,0.2)] min-w-[${SIDEBAR_WIDTH}] px-3.5 py-6 flex flex-col items-center rounded-[16px]`}
+    >
       <ProfilePicture />
       <SidebarTabs.Root defaultValue={currentTab}>
         <SidebarTabs.List className='w-full mt-6 space-y-2'>
