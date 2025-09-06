180bc452c67e213d7c2397e8d9d9ece23431e28a
Squarecat-meow <yozumina@serafuku.moe>
2025-09-06 19:03:14 +0900

    feat(myReservationStatus): 예약 현황 페이지에 달력 추가

--- src/components/pages/myReservationStatus/myReservationStatus.tsx
+++ src/components/pages/myReservationStatus/myReservationStatus.tsx
@@ -1,22 +1,34 @@
 'use client';
 
-import { queries } from '@/src/services/primitives/queries';
-import { useTokenStore } from '@/src/store/useTokenStore';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Dropdown from '../../primitives/Dropdown';
 import LoadingSpinner from '../../primitives/LoadingSpinner';
 import Image from 'next/image';
 import BackBtn from '../../primitives/mypage/BackBtn';
+import { Activity, IMyReservations } from '@/src/types/activityType';
+import ReservationCalendar from './ReservationCalendar';
+import {
+  getMyExperiences,
+  getMyReservationStatus,
+} from '@/src/services/pages/myExperiences/api';
+import { useReservationStore } from '@/src/store/ReservationStore';
+import { format } from 'date-fns';
+import { IReservedSchedule } from '@/src/types/scheduleType';
 
 export default function MyReservationStatusPage() {
-  const [selectedExperience, setSelectedExperience] = useState<string | null>(
-    null
-  );
-  const { accessToken } = useTokenStore();
-  const { data, isPending } = useQuery(
-    queries.myExperiencesOptions(accessToken)
-  );
+  const [activities, setActivities] = useState<IMyReservations | null>();
+
+  useEffect(() => {
+    const fn = async () => {
+      const activitiesList = await getMyExperiences();
+      if (!activitiesList || activitiesList.totalCount === 0)
+        setActivities(null);
+      setActivities(activitiesList);
+    };
+    fn();
+  }, []);
+
+  console.log(activities);
 
   return (
     <section className='flex flex-col items-center gap-8'>
@@ -29,17 +41,10 @@ export default function MyReservationStatusPage() {
           내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.
         </p>
       </div>
-      {isPending ? (
-        <LoadingSpinner />
-      ) : (
+      {activities !== undefined ? (
         <>
-          {data && data.totalCount !== 0 ? (
-            <Dropdown
-              label=''
-              items={data.activities.map((el) => el.title)}
-              value={selectedExperience}
-              onChange={(e) => setSelectedExperience(e)}
-            />
+          {activities !== null && activities.totalCount !== 0 ? (
+            <DropdownAndCalendar data={activities.activities} />
           ) : (
             <div className='space-y-8'>
               <Image
@@ -55,7 +60,44 @@ export default function MyReservationStatusPage() {
             </div>
           )}
         </>
+      ) : (
+        <LoadingSpinner />
       )}
     </section>
+  );
+}
+
+function DropdownAndCalendar({ data }: { data: Activity[] }) {
+  const [selectedExperience, setSelectedExperience] = useState<string | null>(
+    null
+  );
+  const [reservedSchedule, setReservedSchedule] = useState<
+    IReservedSchedule[] | null
+  >(null);
+
+  const calendarDisplayDate = useReservationStore(
+    (state) => state.displayController.dateToDisplay
+  );
+
+  Promise.all(
+    data.map((el) => {
+      return getMyReservationStatus(
+        el.id,
+        format(calendarDisplayDate, 'yyyy'),
+        format(calendarDisplayDate, 'MM')
+      );
+    })
+  ).then((res) => setReservedSchedule(res));
+
+  return (
+    <div>
+      <Dropdown
+        label=''
+        items={data.map((el) => el.title)}
+        value={selectedExperience}
+        onChange={(e) => setSelectedExperience(e)}
+      />
+      <ReservationCalendar schedule={reservedSchedule} />
+    </div>
   );
 }
