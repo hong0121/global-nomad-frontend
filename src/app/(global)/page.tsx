25db02141834fbc23862e6a49f847ee245178ce3
tasi <taegyung.e@gmail.com>
2025-09-08 16:42:38 +0900

    style(main): 메인페이지 레이아웃

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -1,8 +1,11 @@
+import TopBanner from '@/src/components/pages/main/TopBanner';
+
 export default function MainPage() {
   return (
-    <main>
-      <h1>메인 페이지</h1>
-      <p>여기는 홈 화면입니다.</p>
-    </main>
+    <div className='pt-[122px] pb-[136px] bg-[center_top] bg-[100%_auto] bg-[url(/images/MainBgMO.jpg)] bg-repeat-x md:pt-[183px] md:pb-[204px] md:bg-[url(/images/MainBgTB.jpg)] lg:pb-[218px] lg:bg-auto lg:bg-[url(/images/MainBgPC.jpg)]'>
+      <div className='px-6 md:px-0 md:w-[calc(100%-60px)] md:max-w-[1120px] md:mx-auto'>
+        <TopBanner />
+      </div>
+    </div>
   );
 }
