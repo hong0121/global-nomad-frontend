47530b899b715c57d68c0d2fa201211718e9d227
tasi <taegyung.e@gmail.com>
2025-09-09 01:35:11 +0900

    chore(MainBg): 상단 데코레이션 BG 수정

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -2,11 +2,17 @@ import AllExperiencesSection from '@/src/components/pages/main/AllExperiencesSec
 import PopularExperiencesSection from '@/src/components/pages/main/PopularExperiencesSection';
 import SearchBar from '@/src/components/pages/main/SearchBar';
 import TopBanner from '@/src/components/pages/main/TopBanner';
+import { cn } from '@/src/utils/cn';
 
 export default function MainPage() {
   return (
-    <div className='pt-[122px] pb-[136px] bg-[center_top] bg-[100%_auto] bg-[url(/images/MainBgMO.jpg)] bg-repeat-x md:pt-[183px] md:pb-[204px] md:bg-[url(/images/MainBgTB.jpg)] lg:pb-[218px] lg:bg-auto lg:bg-[url(/images/MainBgPC.jpg)]'>
-      <div className='px-6 md:px-0 md:w-[calc(100%-60px)] md:max-w-[1120px] md:mx-auto'>
+    <div
+      className={cn(
+        'pt-[122px] pb-[136px]  bg-linear-to-b from-[#BBDDFF] via-[#F7FBFF] via-27% to-white md:pt-[183px] md:pb-[204px] lg:pb-[218px] ',
+        'before:content-[""] before:absolute before:top-0 before:w-full before:h-full before:bg-[center_top] before:bg-auto before:bg-repeat-x before:bg-[url(/images/MainBgMO.png)] md:before:bg-[url(/images/MainBgTB.png)] lg:before:bg-[url(/images/MainBgPC.png)]'
+      )}
+    >
+      <div className='relative px-6 md:px-0 md:w-[calc(100%-60px)] md:max-w-[1120px] md:mx-auto'>
         <TopBanner />
         <SearchBar />
         <PopularExperiencesSection />
