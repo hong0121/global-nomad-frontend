a715ad27c9673720d7bef1d8f818ec653c45f1a0
taegyung <taegyung.e@gmail.com>
2025-09-10 17:04:59 +0900

    chore(SliderBox): 데이터 없을때 렌더링 UI 추가

--- src/components/pages/main/SliderBox.tsx
+++ src/components/pages/main/SliderBox.tsx
@@ -9,6 +9,7 @@ import { cn } from '@/src/utils/cn';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { getActivities } from '@/src/services/pages/main/api';
 import LoadingSpinner from '@/src/components/primitives/LoadingSpinner';
+import EmptyList from '@/src/components/primitives/EmptyList';
 
 const ArrowCommonStyle =
   'hidden absolute top-1/2 -translate-y-1/2 w-[54px] h-[54px] border border-[rgba(0,0,0,0.3)] bg-white rounded-full z-[1] md:flex md:items-center md:justify-center disabled:opacity-0';
@@ -48,65 +49,71 @@ export default function SliderBox() {
 
   return (
     <div className='relative'>
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={12}
-        slidesPerView={2.5}
-        navigation={{
-          prevEl: '.prev-btn',
-          nextEl: '.next-btn',
-        }}
-        breakpoints={{
-          744: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1200: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-          },
-        }}
-        onReachEnd={() => {
-          if (hasNextPage && !isFetchingNextPage) fetchNextPage();
-        }}
-        className='pt-[14px]! pb-[15px]! px-6! -mx-6! md:pt-4! md:px-[13px]! md:-mx-[13px]! lg:pt-5!'
-      >
-        {pagesData.map((page) =>
-          page.activities.map((activitie) => (
-            <SwiperSlide key={activitie.id}>
-              <MainExperienceCard activity={activitie} />
-            </SwiperSlide>
-          ))
-        )}
-      </Swiper>
+      {pagesData.length === 0 ? (
+        <EmptyList>등록된 체험이 없습니다.</EmptyList>
+      ) : (
+        <>
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={12}
+            slidesPerView={2.5}
+            navigation={{
+              prevEl: '.prev-btn',
+              nextEl: '.next-btn',
+            }}
+            breakpoints={{
+              744: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1200: {
+                slidesPerView: 4,
+                spaceBetween: 24,
+              },
+            }}
+            onReachEnd={() => {
+              if (hasNextPage && !isFetchingNextPage) fetchNextPage();
+            }}
+            className='pt-[14px]! pb-[15px]! px-6! -mx-6! md:pt-4! md:px-[13px]! md:-mx-[13px]! lg:pt-5!'
+          >
+            {pagesData.map((page) =>
+              page.activities.map((activitie) => (
+                <SwiperSlide key={activitie.id}>
+                  <MainExperienceCard activity={activitie} />
+                </SwiperSlide>
+              ))
+            )}
+          </Swiper>
 
-      <button
-        className={cn(
-          ArrowCommonStyle,
-          'prev-btn -left-[15px] lg:-left-[28px]'
-        )}
-      >
-        <Image
-          src='/images/icons/SliderArrow.svg'
-          width={24}
-          height={24}
-          alt='이전으로'
-          className='rotate-180'
-        />
-      </button>
-      <button
-        className={cn(
-          ArrowCommonStyle,
-          'next-btn -right-[15px] lg:-right-[28px]'
-        )}
-      >
-        <Image
-          src='/images/icons/SliderArrow.svg'
-          width={24}
-          height={24}
-          alt='다음으로'
-        />
-      </button>
+          <button
+            className={cn(
+              ArrowCommonStyle,
+              'prev-btn -left-[15px] lg:-left-[28px]'
+            )}
+          >
+            <Image
+              src='/images/icons/SliderArrow.svg'
+              width={24}
+              height={24}
+              alt='이전으로'
+              className='rotate-180'
+            />
+          </button>
+          <button
+            className={cn(
+              ArrowCommonStyle,
+              'next-btn -right-[15px] lg:-right-[28px]'
+            )}
+          >
+            <Image
+              src='/images/icons/SliderArrow.svg'
+              width={24}
+              height={24}
+              alt='다음으로'
+            />
+          </button>
+        </>
+      )}
     </div>
   );
 }
