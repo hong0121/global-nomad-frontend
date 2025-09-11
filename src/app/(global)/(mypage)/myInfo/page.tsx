cfd315fb215a0b796fbe9f75b345416d5f4adca7
tasi <taegyung.e@gmail.com>
2025-09-11 23:06:10 +0900

    style(mypageLayout): css 수정

--- src/app/(global)/(mypage)/myInfo/page.tsx
+++ src/app/(global)/(mypage)/myInfo/page.tsx
@@ -56,7 +56,8 @@ export default function MypageLayout() {
       <section className='w-full pt-[85px] pb-[64px] overflow-hidden md:pt-[120px] md:pb-[54px] h-full'>
         <div
           className={cn(
-            'flex w-[200vw] transition-all md:w-[calc(100%-60px)] md:gap-[30px] md:max-w-[980px] md:mx-auto lg:gap-[50px] -translate-x-1/2 md:translate-x-0 h-auto'
+            'flex w-[200vw] transition-all md:w-[calc(100%-60px)] md:gap-[30px] md:max-w-[980px] md:mx-auto lg:gap-[50px] h-[480px] md:h-auto',
+            isTabOpen && '-translate-x-1/2 md:translate-x-0 h-auto'
           )}
         >
           <div className='w-screen px-6 shrink-0 md:w-[178px] md:px-0 lg:w-[290px]'>
