bfe2333d2fcaa5589510665a70672801fbdef18e
tasi <taegyung.e@gmail.com>
2025-09-11 23:05:56 +0900

    feat(Header): 스크롤시 헤더 모션 추가

--- src/components/primitives/global/Header/Header.tsx
+++ src/components/primitives/global/Header/Header.tsx
@@ -2,15 +2,47 @@
 import LoggingInGnb from '@/src/components/primitives/global/Header/LoggingInGnb';
 import LoggingOutGnb from '@/src/components/primitives/global/Header/LoggingOutGnb';
 import useCurrentUser from '@/src/hooks/useCurrentUser';
+import { cn } from '@/src/utils/cn';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Header() {
   const userInfo = useCurrentUser();
+  const headerRef = useRef<HTMLHeadElement>(null);
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    const headerCurrent = headerRef.current;
+    if (!headerCurrent) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsTop(true);
+        } else {
+          setIsTop(false);
+        }
+      },
+      { threshold: 1 }
+    );
+
+    observer.observe(headerCurrent);
+
+    return () => {
+      observer.unobserve(headerCurrent);
+    };
+  }, []);
 
   return (
-    <header className='absolute top-0 left-0 right-0 z-50'>
-      <div className='flex justify-between items-center max-w-[1520px] h-12 md:h-20 mx-auto px-6 md:px-[30px] lg:px-0 lg:w-[calc(100%-60px)]'>
+    <header className='absolute top-0 left-0 right-0 z-50' ref={headerRef}>
+      <div
+        className={cn(
+          'fixed top-0 left-0 right-0 flex justify-between items-center max-w-[1520px] h-12 md:h-20 mx-auto px-6 md:px-[30px] lg:px-0 lg:w-[calc(100%-60px)] transition-all',
+          !isTop &&
+            'left-2 right-2 top-2 bg-primary-500/10 rounded-md backdrop-blur-2xl lg:px-[30px]'
+        )}
+      >
         <h1>
           <Link href='/' className='w-7 md:w-[174px]'>
             <Image
