d2b1c74b8ee3f58a30cd772607db4615cf3ad7ef
tasi <taegyung.e@gmail.com>
2025-09-12 17:09:10 +0900

    fix(AuthProvider): 마이페이지에서 새로고침시 로그인 페이지로 이동되는 버그 수정

--- src/components/primitives/AuthProvider.tsx
+++ src/components/primitives/AuthProvider.tsx
@@ -5,30 +5,37 @@ import { queries } from '@/src/services/primitives/queries';
 import { useTokenStore } from '@/src/store/useTokenStore';
 import { useQuery } from '@tanstack/react-query';
 import { usePathname, useRouter } from 'next/navigation';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 interface Props {
   children: ReactNode;
 }
 
 export default function AuthProvicder({ children }: Props) {
+  const [isAuthChecked, setIsAuthCheck] = useState<boolean | null>(null);
   const pathname = usePathname();
   const router = useRouter();
   const accessToken = useTokenStore((state) => state.accessToken);
 
   // 리액트쿼리는 액세스 토큰이 있고, 유효한 액세스 토큰을 넘겨주었을 때에만 페칭을 시작함.
-  const { isLoading, data: userInfo } = useQuery({
+  const { isLoading } = useQuery({
     ...queries.userOptions(accessToken),
     retry: false,
     staleTime: Infinity,
   });
 
+  useEffect(() => {
+    if (!!accessToken) {
+      setIsAuthCheck(true);
+    } else {
+      setIsAuthCheck(false);
+    }
+  }, [accessToken]);
+
   // 리다이렉트 분기
   useEffect(() => {
     // 리액트 쿼리 페칭중이라면 return
-    if (isLoading) return;
-
-    const isLoggingIn = !!userInfo;
+    if (isAuthChecked === null || isLoading) return;
 
     const AUTH_PATHS = [
       '/login',
@@ -44,7 +51,7 @@ export default function AuthProvicder({ children }: Props) {
     );
 
     // 로그인 상태일때
-    if (isLoggingIn) {
+    if (isAuthChecked) {
       // auth 페이지(로그인/회원가입) 라우트시
       if (isAuthPath) {
         router.replace('/');
@@ -52,14 +59,14 @@ export default function AuthProvicder({ children }: Props) {
     }
 
     // 로그인 상태가 아닐 때
-    if (!isLoggingIn) {
+    if (!isAuthChecked) {
       // 프로텍트 페이지 (/, /detail, /login, /signup 제외) 접속시
       if (!isPublicPath && !isAuthPath) {
         // 로그인페이지로 리다이렉트, 이때 params에 ?&redirect_uri=현재페이지경로 를 추가해줘서 로그인 완료시 접속시도했던 페이지로 리다이렉트되도록 처리
         router.replace(`/login?redirect_path=${pathname}`);
       }
     }
-  }, [pathname, router, accessToken, isLoading, userInfo]);
+  }, [pathname, router, isLoading, isAuthChecked]);
 
   // 리액트 쿼리 페칭중이라면 로딩 보여주기.
   if (isLoading)
